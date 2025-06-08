539bbb2879f6e28e0e62f6090c7ae6b42391f8dc
arl.2k3 <ariel_pk@outlook.com>
2025-06-08 15:50:55 -0500

    Ruta agregada en 'UsuarioController', Modificacion en 'listaUsuario'

--- src/main/java/madstodolist/controller/UsuarioController.java
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package madstodolist.controller;
 
-import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import madstodolist.service.UsuarioService;
 
 @Controller
 public class UsuarioController {
@@ -16,5 +18,11 @@ public class UsuarioController {
     public String listadoUsuarios(Model model) {
         model.addAttribute("usuarios", usuarioService.findAll());
         return "listaUsuarios";
+    }
+
+    @GetMapping("/registrados/{id}")
+    public String detalleUsuario(@PathVariable(value="id") Long idUsuario, Model model) {
+        model.addAttribute("usuario", usuarioService.findById(idUsuario));
+        return "detalleUsuario";
     }
 }
