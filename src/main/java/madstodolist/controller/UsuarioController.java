869ae6c631d47b892d0253e936cf050dc84f91fb
arl.2k3 <ariel_pk@outlook.com>
2025-06-08 16:42:36 -0500

    Error NavBar corregido dentro de 'detalleUsuario' y actualizacion en 'usuarioController'

--- src/main/java/madstodolist/controller/UsuarioController.java
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import madstodolist.authentication.ManagerUserSession;
 import madstodolist.service.UsuarioService;
 
 @Controller
@@ -14,15 +15,22 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private ManagerUserSession managerUserSession;
+
     @GetMapping("/registrados")
     public String listadoUsuarios(Model model) {
+        Long idUsuario = managerUserSession.usuarioLogeado();
+        model.addAttribute("usuario", idUsuario != null ? usuarioService.findById(idUsuario) : null);
         model.addAttribute("usuarios", usuarioService.findAll());
         return "listaUsuarios";
     }
 
     @GetMapping("/registrados/{id}")
     public String detalleUsuario(@PathVariable(value="id") Long idUsuario, Model model) {
-        model.addAttribute("usuario", usuarioService.findById(idUsuario));
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        model.addAttribute("usuario", idUsuarioLogeado != null ? usuarioService.findById(idUsuarioLogeado) : null);
+        model.addAttribute("usuarioDetalle", usuarioService.findById(idUsuario));
         return "detalleUsuario";
     }
 }
